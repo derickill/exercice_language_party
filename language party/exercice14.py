27227194eb9a47d50192d8c676b3ffd2d1e353b0
derickill <derick.attoubou@epita.fr>
2025-01-06 21:37:58 +0100

    Update exercice14.py

--- language party/exercice14.py
+++ language party/exercice14.py
@@ -1,6 +1,9 @@
 nbrr = int(input("entrer votre nombre ici"))
 a = nbrr-1
-while a!=0:
-    nbrr = nbrr*a
-    a -= 1
-print(f"le resultat est {nbrr}")
+if nbrr ==0:
+    print("le resultat est 1")
+else:
+    while a!=0:
+        nbrr = nbrr*a
+        a -= 1
+    print(f"le resultat est {nbrr}")
