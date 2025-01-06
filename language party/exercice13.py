2289d5459fc687450512fdfb36d52c2191ee5791
derickill <derick.attoubou@epita.fr>
2025-01-06 21:33:19 +0100

    Update exercice13.py

--- language party/exercice13.py
+++ language party/exercice13.py
@@ -1,4 +1,4 @@
-nob = input("netrer vos nombre separe par une virgule")
+nob = input("Entrez vos nombres, séparés par une virgule")
 liste_de_nombre = [float(i) for i in nob.split(sep=",")]
 
 print(sorted(liste_de_nombre))
